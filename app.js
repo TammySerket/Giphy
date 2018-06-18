624fc76d675af02688767b6e574acc64aeb86118
Tamara Hinojosa Salinas <tamara.hi.sa@hotmail.com>
2018-06-18 18:30:51 -0400

    se crea html css

---
+++ app.js
@@ -0,0 +1,4 @@
+const containerUrl = document.getElementById("")
+const containerType = document.getElementById("")
+const containerDataType = document.getElementById("")
+const containerData = document.getElementById("")
